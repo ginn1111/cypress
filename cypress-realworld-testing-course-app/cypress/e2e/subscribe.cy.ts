2f4ea77e01471e9fdf444d4f3eac75b37cfbc63e
thuanpv <thuanpv@payme.vn>
2023-08-21 17:25:50 +0700

    narrow selector, navigate multiple page, checking pathname with location API

--- cypress-realworld-testing-course-app/cypress/e2e/subscribe.cy.ts
+++ cypress-realworld-testing-course-app/cypress/e2e/subscribe.cy.ts
@@ -15,7 +15,7 @@ describe("Newsletter Subscribe Form", () => {
     cy.getByData("success-message").should("not.exist")
   })
 
-  it.only("does NOT allow an exist email address", () => {
+  it("does NOT allow an exist email address", () => {
     cy.getByData("email-input").type("john@example.com")
     cy.getByData("submit-button").click()
     cy.getByData("server-error-message")
